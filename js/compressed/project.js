5b2bd2cc8cb0a39ecd0b816956811e577948d610
henry van dusen <hgv2102@columbia.edu>
2016-03-27 14:59:26 -0400

    home page

---
+++ js/compressed/project.js
@@ -0,0 +1,1 @@
+!function($){$(document).ready(function(){"-1"!==$("#myCanvas").css("z-index")&&$("#myCanvas").css("z-index","-1")})}(jQuery);
--- js/project.js
+++ js/project.js
@@ -1,5 +1,6 @@
 (function ($) {
 
 	$(document).ready(function(){
+		$('#myCanvas').css('z-index') !== '-1' && $('#myCanvas').css('z-index','-1');
 	});
 })(jQuery);
