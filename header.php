f012049b3c0b5fed5759c47f47a1b858a57f9d7b
henry van dusen <hgv2102@columbia.edu>
2016-03-27 15:12:36 -0400

    title

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, height=device-height,initial-scale=0.5, maximum-scale=1">
-	<title><?php print trim(wp_title('', false)); ?><?php if(wp_title('', false)) { echo ' | '; } ?><?php bloginfo('name'); ?></title>
+	<title><?php bloginfo('name'); ?></title>
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 	<script src="<?php bloginfo('template_directory'); ?>/js/compressed/modernizr.js"></script>
